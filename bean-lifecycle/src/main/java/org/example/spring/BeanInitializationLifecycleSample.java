f76a81ed1bb148b45d965f02128477ef4c74724b
zhengsj <zhengsj@akulaku.com>
2020-11-10 23:42:18 +0800

    spring 生命周期

--- bean-lifecycle/src/main/java/org/example/spring/BeanInitializationLifecycleSample.java
+++ bean-lifecycle/src/main/java/org/example/spring/BeanInitializationLifecycleSample.java
@@ -2,13 +2,18 @@ package org.example.spring;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 
@@ -27,6 +32,21 @@ import javax.annotation.PostConstruct;
  *
  *      {@link AbstractAutowireCapableBeanFactory#invokeInitMethods(java.lang.String, java.lang.Object, org.springframework.beans.factory.support.RootBeanDefinition)}
  *
+ *
+ *    100 | Spring Bean初始化后阶段：BeanPostProcessor
+ *
+ *      {@link BeanPostProcessor#postProcessAfterInitialization(java.lang.Object, java.lang.String)}
+ *         {@link AbstractAutowireCapableBeanFactory#applyBeanPostProcessorsAfterInitialization(java.lang.Object, java.lang.String)} 应用
+ *
+ *
+ *    101 | Spring Bean初始化完成阶段：SmartInitializingSingleton (spring 4.1以上)
+ *      {@link SmartInitializingSingleton}
+ *      {@link AbstractApplicationContext#refresh()}
+ *      -- {@link AbstractApplicationContext#finishBeanFactoryInitialization(org.springframework.beans.factory.config.ConfigurableListableBeanFactory)}
+ *      -- -- {@link ConfigurableListableBeanFactory#preInstantiateSingletons()}
+ *
+ *
+ *
  * @author zhengshijun
  * @version created on 2020/11/9.
  */
@@ -36,21 +56,27 @@ public class BeanInitializationLifecycleSample {
 	public static void main(String[] args) {
 
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
 		beanFactory.addBeanPostProcessor(new CustomBeanPostProcessor());
 		// 添加处理 {@link javax.annotation.PostConstruct}
 		beanFactory.addBeanPostProcessor(new CommonAnnotationBeanPostProcessor());
-		AnnotatedBeanDefinitionReader beanDefinitionReader = new AnnotatedBeanDefinitionReader(beanFactory);
+		XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
 
-		//beanDefinitionReader.registerBean(InitSampleEntity.class);
+		xmlBeanDefinitionReader.loadBeanDefinitions(new ClassPathResource("META-INF/beans.xml"));
 
 		BeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(InitSampleEntity.class).setInitMethodName("init").getBeanDefinition();
 		beanFactory.registerBeanDefinition("InitSampleEntity",beanDefinition);
+        // SmartInitializingSingleton
+		beanFactory.preInstantiateSingletons();
+
 		System.out.println(beanFactory.getBean(InitSampleEntity.class));
+
+
 
 	}
 
 
-	static class  InitSampleEntity implements InitializingBean {
+	static class  InitSampleEntity implements InitializingBean,SmartInitializingSingleton {
 		private String name;
 
 		@Override
@@ -73,6 +99,11 @@ public class BeanInitializationLifecycleSample {
 					"name='" + name + '\'' +
 					'}';
 		}
+
+		@Override
+		public void afterSingletonsInstantiated() {
+			System.out.println("afterSingletonsInstantiated");
+		}
 	}
 
 	static class CustomBeanPostProcessor implements BeanPostProcessor {
@@ -85,6 +116,13 @@ public class BeanInitializationLifecycleSample {
 
 			// 等于空会终止后面的 BeanPostProcessor
 			return bean;
+		}
+
+		@Override
+		public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+			System.out.println("postProcessAfterInitialization");
+			return null;
+
 		}
 	}
 }
